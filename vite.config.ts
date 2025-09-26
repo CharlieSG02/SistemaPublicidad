2ff2bb335ce66aa515a97aa318a556179931f258
CharlieSG02 <okmijn@outlook.es>
2025-09-26 16:25:53 -0500

    Build for GitHub Pages with dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
-  
+  build: {
+    outDir: 'docs'  // ← Cambia dist por docs
+  }
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
